d2f7dffc15aee3fa786a8bd0b98b9733436102b8
Josh Sanford <joshsanford1121@gmail.com>
2023-02-04 14:28:59 -0600

    chore: create a very good betchya project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.betchya
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:betchya/app/app.dart';
+import 'package:betchya/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
