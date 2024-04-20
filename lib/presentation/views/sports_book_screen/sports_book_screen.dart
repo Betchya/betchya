4e74cfca2915d782e24efb1944a4daaec00cc0f1
Jonathan Rockett <jonathansrockett@gmail.com>
2024-04-19 19:02:00 -0700

    get branch to github

--- lib/presentation/views/sports_book_screen/sports_book_screen.dart
+++ lib/presentation/views/sports_book_screen/sports_book_screen.dart
@@ -4,6 +4,52 @@ import 'package:betchya/presentation/views/sports_book_screen/widgets/user_rewar
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+final HttpLink mlbHttpLink = HttpLink('https://4ujbpveyubd2bn37rbnltiliyy.appsync-api.us-west-2.amazonaws.com/graphql', 
+  defaultHeaders: {
+    'x-api-key': 'da2-lg6tat5g4zaxjc2evzkbytw2oe',
+},);
+
+final ValueNotifier<GraphQLClient> mlbClient = ValueNotifier<GraphQLClient>(
+  GraphQLClient(
+    link: mlbHttpLink,
+    cache: GraphQLCache(),
+  ),
+);
+
+const String mlbQuery = '''
+query listMLBBettingEvents {
+  listMLBBettingEvents {
+    items {
+      BettingEventID
+      AwayRotationNumber
+      AwayTeam
+      AwayTeamID
+      AwayTeamScore
+      BettingEventType
+      BettingEventTypeID
+      BettingMarkets
+      Created
+      GameID
+      GameStartTime
+      GameStatus
+      GlobalAwayTeamID
+      GlobalGameID
+      GlobalHomeTeamID
+      HomeTeam
+      HomeTeamID
+      HomeTeamScore
+      Name
+      Quarter
+      Season
+      StartDate
+      TotalScore
+      Updated
+    }
+  }
+}
+''';
 
 class SportsbookScreen extends StatefulWidget {
   const SportsbookScreen({super.key});
@@ -35,124 +81,139 @@ class _SportsbookScreenState extends State<SportsbookScreen> {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              backgroundColor: const Color(0xff2C1D57),
-              toolbarHeight: screenHeight * .3,
-              title: UserRewardsWidget(
-                changeCategory: () {
-                  setState(() {
-                    currentCategory = gameCategory;
-                  });
-                },
-              ),
-              automaticallyImplyLeading: false,
-              floating: true,
-              snap: true,
-            ),
-          ];
-        },
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: screenHeight * .015,
-              ),
-              SizedBox(
-                height: screenHeight * .17,
-                width: screenWidth * .9,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: const Image(
-                    image: AssetImage('assets/images/star-gazing-gifts2.png'),
-                    fit: BoxFit.fill,
-                  ),
+    return GraphQLProvider(
+      client: mlbClient,
+      child: Scaffold(
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                backgroundColor: const Color(0xff2C1D57),
+                toolbarHeight: screenHeight * .3,
+                title: UserRewardsWidget(
+                  changeCategory: () {
+                    setState(() {
+                      currentCategory = gameCategory;
+                    });
+                  },
                 ),
+                automaticallyImplyLeading: false,
+                floating: true,
+                snap: true,
               ),
-              SizedBox(
-                height: screenHeight * .015,
-              ),
-              Center(
+            ];
+          },
+          body: Query(
+            options: QueryOptions(
+                document: gql(mlbQuery),
+                variables: const <String, dynamic>{'x-api-key': 'da2-lg6tat5g4zaxjc2evzkbytw2oe'}),
+            builder: (result, {fetchMore, refetch}) { 
+
+              final items = result.data?['listMLBBettingEvents']['items'][0];
+
+              return SingleChildScrollView(
                 child: Column(
                   children: [
-                    CarouselSlider.builder(
-                      options: CarouselOptions(
-                        viewportFraction: 1,
-                        height: screenHeight * .17,
-                        onPageChanged: (index, reason) =>
-                            setState(() => activeIndex = index),
-                      ),
-                      itemCount: urlImages.length,
-                      itemBuilder: (context, index, realIndex) {
-                        final urlImage = urlImages[index];
-                        return SizedBox(
-                          width: screenWidth * .9,
-                          // color: Colors.grey,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: urlImage,
-                          ),
-                        );
-                      },
-                    ),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    AnimatedSmoothIndicator(
-                      activeIndex: activeIndex,
-                      count: urlImages.length,
-                      effect: const ScrollingDotsEffect(
-                        dotHeight: 9,
-                        dotWidth: 9,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                child: Container(
-                  height: screenHeight * .06,
-                  color: Colors.black12,
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(
-                      screenWidth * .043,
-                      0,
-                      screenWidth * .043,
-                      0,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '$gameCategory Games',
+                    
+                    SizedBox(
+                      height: screenHeight * .17,
+                      width: screenWidth * .9,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Text(
+                          '${items["Name"]} \nGame Start: ${items["GameStartTime"]}',
                           style: const TextStyle(
                             fontSize: 18,
                             color: Colors.black,
                             fontWeight: FontWeight.w500,
-                          ),
+                          )
                         ),
-                        Text(
-                          'More $gameCategory',
-                          style: const TextStyle(
-                            fontSize: 18,
-                            color: Colors.black,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
+                    SizedBox(
+                      height: screenHeight * .015,
+                    ),
+                    Center(
+                      child: Column(
+                        children: [
+                          CarouselSlider.builder(
+                            options: CarouselOptions(
+                              viewportFraction: 1,
+                              height: screenHeight * .17,
+                              onPageChanged: (index, reason) =>
+                                  setState(() => activeIndex = index),
+                            ),
+                            itemCount: urlImages.length,
+                            itemBuilder: (context, index, realIndex) {
+                              final urlImage = urlImages[index];
+                              return SizedBox(
+                                width: screenWidth * .9,
+                                // color: Colors.grey,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: urlImage,
+                                ),
+                              );
+                            },
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          AnimatedSmoothIndicator(
+                            activeIndex: activeIndex,
+                            count: urlImages.length,
+                            effect: const ScrollingDotsEffect(
+                              dotHeight: 9,
+                              dotWidth: 9,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      child: Container(
+                        height: screenHeight * .06,
+                        color: Colors.black12,
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(
+                            screenWidth * .043,
+                            0,
+                            screenWidth * .043,
+                            0,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '$gameCategory Games',
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                'More $gameCategory',
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenHeight * .75,
+                      child: const GamesWidget(),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(
-                height: screenHeight * .75,
-                child: const GamesWidget(),
-              ),
-            ],
+              );
+            }
           ),
         ),
       ),
--- lib/presentation/views/sports_book_screen/widgets/games_widget.dart
+++ lib/presentation/views/sports_book_screen/widgets/games_widget.dart
@@ -5,6 +5,53 @@ import 'package:betchya/other_models/games.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+
+final HttpLink mlbHttpLink = HttpLink('https://4ujbpveyubd2bn37rbnltiliyy.appsync-api.us-west-2.amazonaws.com/graphql', 
+  defaultHeaders: {
+    'x-api-key': 'da2-lg6tat5g4zaxjc2evzkbytw2oe',
+},);
+
+final ValueNotifier<GraphQLClient> mlbClient = ValueNotifier<GraphQLClient>(
+  GraphQLClient(
+    link: mlbHttpLink,
+    cache: GraphQLCache(),
+  ),
+);
+
+const String mlbQuery = '''
+query listMLBBettingEvents {
+  listMLBBettingEvents {
+    items {
+      BettingEventID
+      AwayRotationNumber
+      AwayTeam
+      AwayTeamID
+      AwayTeamScore
+      BettingEventType
+      BettingEventTypeID
+      BettingMarkets
+      Created
+      GameID
+      GameStartTime
+      GameStatus
+      GlobalAwayTeamID
+      GlobalGameID
+      GlobalHomeTeamID
+      HomeTeam
+      HomeTeamID
+      HomeTeamScore
+      Name
+      Quarter
+      Season
+      StartDate
+      TotalScore
+      Updated
+    }
+  }
+}
+''';
 
 class GamesWidget extends StatefulWidget {
   const GamesWidget({super.key});
@@ -27,137 +74,140 @@ class _GamesWidgetState extends State<GamesWidget> {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
-    return FutureBuilder<List<Game>>(
-      future: APIGetGames().getGameList(gameCategory),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-            padding: const EdgeInsets.fromLTRB(8, 10, 8, 10),
-            shrinkWrap: true,
-            itemCount: snapshot.data?.length,
-            itemBuilder: (context, index) {
-              final game = snapshot.data![index];
+    return GraphQLProvider(
+      client: mlbClient,
+      child:  FutureBuilder<List<Game>>(
+        future: APIGetGames().getGameList(gameCategory),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              padding: const EdgeInsets.fromLTRB(8, 10, 8, 10),
+              shrinkWrap: true,
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index) {
+                final game = snapshot.data![index];
 
-              return Card(
-                elevation: 5,
-                clipBehavior: Clip.antiAlias,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: screenHeight * .01,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          SizedBox.square(
-                            dimension: screenWidth * .2,
-                            child: CachedNetworkImage(
-                              imageUrl: game.teamLogo1,
-                              progressIndicatorBuilder:
-                                  (context, url, progress) => Center(
-                                child: CircularProgressIndicator(
-                                  value: progress.progress,
+                return Card(
+                  elevation: 5,
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: screenHeight * .01,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SizedBox.square(
+                              dimension: screenWidth * .2,
+                              child: CachedNetworkImage(
+                                imageUrl: game.teamLogo1,
+                                progressIndicatorBuilder:
+                                    (context, url, progress) => Center(
+                                  child: CircularProgressIndicator(
+                                    value: progress.progress,
+                                  ),
                                 ),
+                                errorWidget: (context, url, error) =>
+                                    const Icon(Icons.error),
+                                fit: BoxFit.cover,
                               ),
-                              errorWidget: (context, url, error) =>
-                                  const Icon(Icons.error),
-                              fit: BoxFit.cover,
                             ),
-                          ),
-                          const Text('VS'),
-                          SizedBox.square(
-                            dimension: screenWidth * .2,
-                            child: CachedNetworkImage(
-                              imageUrl: game.teamLogo2,
-                              progressIndicatorBuilder:
-                                  (context, url, progress) => Center(
-                                child: CircularProgressIndicator(
-                                  value: progress.progress,
+                            const Text('VS'),
+                            SizedBox.square(
+                              dimension: screenWidth * .2,
+                              child: CachedNetworkImage(
+                                imageUrl: game.teamLogo2,
+                                progressIndicatorBuilder:
+                                    (context, url, progress) => Center(
+                                  child: CircularProgressIndicator(
+                                    value: progress.progress,
+                                  ),
                                 ),
+                                errorWidget: (context, url, error) =>
+                                    const Icon(Icons.error),
+                                fit: BoxFit.cover,
                               ),
-                              errorWidget: (context, url, error) =>
-                                  const Icon(Icons.error),
-                              fit: BoxFit.cover,
                             ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: screenHeight * .01,
-                      ),
-                      Text(game.description),
-                      SizedBox(
-                        height: screenHeight * .01,
-                      ),
-                      Text(
-                        'Betting Line: ${game.betLine} ',
-                        style: const TextStyle(color: Colors.deepPurple),
-                      ),
-                      SizedBox(
-                        height: screenHeight * .01,
-                      ),
-                      SizedBox(
-                        width: screenWidth * 0.8,
-                        child: TextFormField(
-                          controller: amountController,
-                          keyboardType: const TextInputType.numberWithOptions(
-                            decimal: true,
-                          ),
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.attach_money),
-                            border: OutlineInputBorder(),
-                            labelText: 'Amount',
-                          ),
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly,
                           ],
                         ),
-                      ),
-                      SizedBox(
-                        height: screenHeight * .01,
-                      ),
-                      SizedBox(
-                        width: screenWidth * .8,
-                        height: screenHeight * .05,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xff00B498),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
+                        SizedBox(
+                          height: screenHeight * .01,
+                        ),
+                        Text(game.description),
+                        SizedBox(
+                          height: screenHeight * .01,
+                        ),
+                        Text(
+                          'Betting Line: ${game.betLine} ',
+                          style: const TextStyle(color: Colors.deepPurple),
+                        ),
+                        SizedBox(
+                          height: screenHeight * .01,
+                        ),
+                        SizedBox(
+                          width: screenWidth * 0.8,
+                          child: TextFormField(
+                            controller: amountController,
+                            keyboardType: const TextInputType.numberWithOptions(
+                              decimal: true,
                             ),
-                          ),
-                          onPressed: () => {
-                            // APIAddBets().addBet(
-                            //   game.description,
-                            //   game.betLine,
-                            //   game.teamLogo1,
-                            //   game.teamLogo2,
-                            //   amountController.text,
-                            //   game.date,
-                            // ),
-                            setState(() {}),
-                          },
-                          child: const Text(
-                            'Place Bet',
-                            style: TextStyle(fontSize: 15),
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.attach_money),
+                              border: OutlineInputBorder(),
+                              labelText: 'Amount',
+                            ),
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                            ],
                           ),
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: screenHeight * .01,
+                        ),
+                        SizedBox(
+                          width: screenWidth * .8,
+                          height: screenHeight * .05,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xff00B498),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            onPressed: () => {
+                              // APIAddBets().addBet(
+                              //   game.description,
+                              //   game.betLine,
+                              //   game.teamLogo1,
+                              //   game.teamLogo2,
+                              //   amountController.text,
+                              //   game.date,
+                              // ),
+                              setState(() {}),
+                            },
+                            child: const Text(
+                              'Place Bet',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              );
-            },
-          );
-        } else {
-          return const CircularProgressIndicator();
-        }
-      },
+                );
+              },
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
