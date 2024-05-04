09caa5390c2e5a62345c99455d74780305f2a01c
Josh-Sanford <52753338+Josh-Sanford@users.noreply.github.com>
2024-05-04 10:39:52 -0700

    Chore/create amplify gen2 project (#47)
    * chore: delete current amplify
    
    * chore: generate amplify gen 2 project with npm create amplify@latest

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -4,7 +4,7 @@ import 'dart:developer';
 // import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:betchya/amplify_configuration.dart';
+// import 'package:betchya/amplify_configuration.dart';
 // import 'package:betchya/models/ModelProvider.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
@@ -30,7 +30,7 @@ Future<void> _configureAmplify() async {
 
     await Amplify.addPlugins([AmplifyAuthCognito()]);
 
-    await Amplify.configure(amplifyconfig);
+    // await Amplify.configure(amplifyconfig);
   } on Exception catch (e) {
     safePrint('An error occurred configuring Amplify: $e');
   }
