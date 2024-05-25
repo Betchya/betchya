56ed6a9190d0b92d4cdbe81be39a19a339ba6d69
Jonathan Rockett <jonathansrockett@gmail.com>
2024-05-25 13:45:44 -0700

    added amplifyconfig file for usage of backend aws graphql/appsync

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,19 @@
+const amplifyconfig = '''
+{
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "SportsDataIOBettingEvents": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://77ibeul5kre25jlg7btnlnqyj4.appsync-api.us-west-2.amazonaws.com/graphql",
+                    "region": "us-west-2",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-2rwuggqx6zbulmt7d7vrvy3yrq"
+                }
+            }
+        }
+    }
+}
+''';
